3679a7ee61f7512313a1af9d4d29b504965c33a2
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2026-03-22 18:29:03 -0700

    updated scripts

--- scripts/utils/start.sh
+++ scripts/utils/start.sh
@@ -1,4 +1,5 @@
 mkdir -p ~/Documents/Repos/ &> /dev/null;
+cd ~/Documents/Repos/
 git clone https://github.com/QuantumZenoSix/Dev-Environment.git;
 cd Dev-Environment;
 bash ./scripts/utils/selector.sh
